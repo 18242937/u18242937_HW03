5de0ad29330f5c0e3e8dcf8e20aee857f2bcbe98
18242937 <u18242937@tuks.co.za>
2022-06-07 23:24:12 +0200

    Add project files.

---
+++ u18242937_HW3/Controllers/FileController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+using u18242937_HW3.Models;
+
+namespace u18242937_HW3.Controllers
+{
+    public class FileController : Controller
+    {
+        // GET: File
+        public ActionResult Index()
+        {
+            string[] filePaths = Directory.GetDirectories(Server.MapPath("~/App_Data/Uploads"));
+
+            List<FileModel> files = new List<FileModel>();
+
+            foreach(string filePath in filePaths)
+            {
+                files.Add(new FileModel { FileName = Path.GetFileName(filePath) });
+            }
+            return View(files);
+        }
+    }
+}
---
+++ u18242937_HW3/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+using u18242937_HW3.Models;
+
+namespace u18242937_HW3.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        [HttpGet]
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase files)
+        {
+            if(files != null && files.ContentLength > 0)
+            {
+                var fileName = Path.GetFileName(files.FileName);
+
+                var path = Path.Combine(Server.MapPath("~/App_Data/Uploads"), fileName);
+
+                files.SaveAs(path);
+            }
+
+
+            return RedirectToAction("AboutMe");
+            
+
+        }
+        public ActionResult AboutMe()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+
+    }
+}
